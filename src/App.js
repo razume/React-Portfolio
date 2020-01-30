73f946a7ba7c15ba724d174c6356f65897aa064f
Leighton A Pitman <leightonpitman14@gmail.com>
2020-01-29 20:06:00 -0500

    start work on header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/Header';
 import './App.css';
 
+const API = 'https://acme-users-api-rev.herokuapp.com/api';
+
+const fetchUser = async () => {
+  const storage = window.localStorage;
+  const userId = storage.getItem('userId');
+  if (userId) {
+    try {
+      return (await axios.get(`${API}/users/detail/${userId}`)).data;
+    } catch (ex) {
+      storage.removeItem('userId');
+      return fetchUser();
+    }
+  }
+  const user = (await axios.get(`${API}/users/random`)).data;
+  storage.setItem('userId', user.id);
+  return user;
+};
+
 function App() {
-  return <div>Hello Partner</div>;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetchUser().then(user => {
+      setUser(user);
+    });
+  }, []);
+  console.log(user);
+
+  return (
+    <div>
+      <Header user={user} />
+    </div>
+  );
 }
 
 export default App;
